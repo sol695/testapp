221d3889de986ec17c512736c2d33f3f86529698
Sol695 <isk.ismagilof2011@yandex.ru>
2019-06-30 21:37:31 +0300

    asdkf

---
+++ complete js/script.js
@@ -0,0 +1,22 @@
+console.log(2);
+
+
+//*
+//?
+//!
+//
+
+console.log('asdf');
+
+// function addWrod(a,b){
+//     let l = 1;
+//     let s = 2;
+//    return a + b;
+// }
+// addWrod(4,5);
+
+
+function ld() {
+    return;
+}
+ld(2);
